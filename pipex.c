ebf460f6625e9c3b14503e73c1aef39e6789832b
jbergfel <jpedrobacker@outlook.com>
2023-11-29 04:39:29 -0300

    a

--- pipex.c
+++ pipex.c
@@ -6,7 +6,13 @@
 /*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 11:38:18 by jbergfel          #+#    #+#             */
-/*   Updated: 2023/11/28 11:38:19 by jbergfel         ###   ########.fr       */
+/*   Updated: 2023/11/29 04:39:00 by jbergfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+
+int main(int arc, char **arv)
+{
+	
+}
